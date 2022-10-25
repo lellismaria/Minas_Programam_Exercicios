fb381f958ccdcbec54f002e46d503930168eaa83
Maria Emília <100357009+lellismaria@users.noreply.github.com>
2022-10-25 15:57:17 -0300

    Aula 2 ✅

---
+++ JS_Velocidade_Media.js
@@ -0,0 +1,9 @@
+//Crie um algoritmo que calcule a velocidade média de um veículo e retorne com resultado. 
+//Fórmula: Vm(Velocidade média) = distância/tempo 
+
+let distMetros = 500;
+let temposeg = 20;
+
+let velocidademedia = distância / tempo;
+
+console.log("O resultado da velocidade média é: velocidademedia" + m/s);
