89a990b0f88ec41ea4b7350d24acd8995dfa4cc0
Maria Emília <100357009+lellismaria@users.noreply.github.com>
2022-10-25 15:40:52 -0300

    Aula 4 ✅

--- JS_Impar_Par.js
+++ JS_Impar_Par.js
@@ -1,4 +1,9 @@
-//Exercício para saber se o número X é par ou impar
+/*
+  Crie um programa que verifique se um determinado número é impar ou par!
+  Caso seja ímpar retorne "O número X é ímpar"
+  Caso seja par retorne "O número X é ímpar"
+  Dica do sucesso: todo número par é divisível por 2.
+*/
 
 var n = prompt("Digite um numero");
 var total=n/2;
